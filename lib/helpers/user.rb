e9f0e439480ddb92c6cdddf233314a499f34d2c4
Lachlan Hardy <lachlan@lachstock.com.au>
2010-02-15 11:13:40 +1100

    adding linc's refactor

--- lib/helpers/user.rb
+++ lib/helpers/user.rb
@@ -1,26 +1,24 @@
 # This helper ensures we have a proper, authenticated, and signed up user.
 def auth
-  if gmail_user && !gmail_user.kind_of?(Warden::GoogleApps::User)
-    # we are logged in with a proper, setup user. all good
-  elsif gmail_user
-    # we have a un setup user. sent to setup page
-    redirect '/signup/'
-  else
-    # not logged in. Authenticate
-    unless env['warden'].authenticate!
-      throw(:warden)
-    end
-  end
+ if logged_in?
+   # do nothing
+ else
+   unless env['warden'].authenticate!
+     throw(:warden)
+   end
+ end
 end
+
+# This helper checks if we are correctly logged in and signed up. If we are logged in but not signed up, forces us to complete the sign up
 def logged_in?
  if gmail_user && !gmail_user.kind_of?(Warden::GoogleApps::User)
-   # fully logged in and signed up
-   return true
+  # fully logged in and signed up
+  return true
  elsif gmail_user
-   # not signed up. Send to sign up form.
-   raise(redirect, '/signup/')
+  # not signed up. Send to sign up form.
+  raise(redirect, '/signup/')
  else
-   false
+  false
  end
 end
 
